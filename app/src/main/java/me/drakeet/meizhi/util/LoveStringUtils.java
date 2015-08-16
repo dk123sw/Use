fa87fa2654825ef616c8d2f6a7d83d73ab6ce4e2
drakeet <drakeet.me@gmail.com>
2015-08-16 15:06:35 +0800

    Fix video no pic exception.

--- app/src/main/java/me/drakeet/meizhi/util/LoveStringUtils.java
+++ app/src/main/java/me/drakeet/meizhi/util/LoveStringUtils.java
@@ -6,13 +6,10 @@ package me.drakeet.meizhi.util;
 public class LoveStringUtils {
 
     public static String getVideoPreviewImageUrl(String resp) {
-        int s0 = resp.indexOf("<h1>休息视频</h1>");
-        if (s0 == -1) return null;
-
-        int s1 = resp.indexOf("<img", s0);
-        int s2 = resp.indexOf("http:", s1);
-        int e2 = resp.indexOf(".jpg", s2) + ".jpg".length();
-
+        int s0 = resp.indexOf("<h1>休息视频</h1>"); if (s0 == -1) return null;
+        int s1 = resp.indexOf("<img", s0); if (s1 == -1) return null;
+        int s2 = resp.indexOf("http:", s1); if (s2 == -1) return null;
+        int e2 = resp.indexOf(".jpg", s2) + ".jpg".length(); if (e2 == -1) return null;
         return resp.substring(s2, e2);
     }
 }
